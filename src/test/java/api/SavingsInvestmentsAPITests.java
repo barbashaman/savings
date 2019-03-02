941f31c719ba1283a587ba135dcee237e304ba06
matheus.barbachan <matheus.barbachan@ilegra.com>
2019-03-02 09:57:55 -0300

    Primeira versão de teste de API

---
+++ src/test/java/api/SavingsInvestmentsAPITests.java
@@ -0,0 +1,25 @@
+package api;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.core.IsCollectionContaining.hasItems;
+
+public class SavingsInvestmentsAPITests {
+
+    @Test
+    public void savingsInvestmentsGETTest() {
+
+
+        when()
+                .get("http://5b847b30db24a100142dce1b.mockapi.io/api/v1/simulador")
+                .then()
+                .statusCode(200)
+                .body(
+                        "id", equalTo(1),
+                        "meses", hasItems("112", "124", "136", "148"),
+                        "valor", hasItems("2.802", "3.174", "3.564", "3.971"));
+
+    }
+}
